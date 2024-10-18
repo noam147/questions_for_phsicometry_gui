f0c96b8831c4237cc7fc625a2d6028ca4da50e27
noam147 <noamar1234567@gmail.com>
2024-10-18 17:06:30 +0300

    add simulation 6 chapters

--- clientForQuestions2.0/chaptersQuestionsMenu.cs
+++ clientForQuestions2.0/chaptersQuestionsMenu.cs
@@ -30,6 +30,7 @@ namespace clientForQuestions2._0
         private void chapterButton_Click(object sender, EventArgs e)
         {
             this.continueButton.Enabled = true;
+            this.downloadExreciseButton.Enabled = true;
             this.chosen_chapter = ((Button)sender).Text.ToString();
 
             this.chapterButton1.BackColor = Color.White;
@@ -38,22 +39,31 @@ namespace clientForQuestions2._0
 
             ((Button)sender).BackColor = Color.LightBlue; // show which category is chosen
         }
-
-        private void continueButton_Click(object sender, EventArgs e)
+        private List<dbQuestionParmeters> getQuestionsBasedOnChapter()
         {
             List<dbQuestionParmeters> questions;
-            switch (chosen_chapter) {
+            switch (chosen_chapter)
+            {
                 case "חשיבה מילולית":
-                    questions = OperationsAndOtherUseful.sendChapter_hebrew_Questions(); 
+                    questions = OperationsAndOtherUseful.sendChapter_hebrew_Questions();
                     break;
                 case "חשיבה כמותית":
                     questions = OperationsAndOtherUseful.sendChapter_math_Questions();
                     break;
                 case "אנגלית":
-                    questions = OperationsAndOtherUseful.sendChapter_english_Questions(); 
+                    questions = OperationsAndOtherUseful.sendChapter_english_Questions();
                     break;
                 default:
-                    return; // no chapter is selected
+                    return null; // no chapter is selected
+            }
+            return questions;
+        }
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            List<dbQuestionParmeters> questions = getQuestionsBasedOnChapter();
+            if (questions == null)
+            {
+                return;
             }
 
             QuestionsToPdf.Questions2Pdf(questions);
@@ -66,6 +76,41 @@ namespace clientForQuestions2._0
 
             c.Show();
             this.Close();
+        }
+
+        private void downloadExreciseButton_Click(object sender, EventArgs e)
+        {
+            List<dbQuestionParmeters> questions = getQuestionsBasedOnChapter();
+            if (questions == null)
+            {
+                return;
+            }
+            HtmlConvertOptionsMenu n = new HtmlConvertOptionsMenu(questions,true);
+            n.Show();
+        }
+
+        private void simulationDownloadButton_Click(object sender, EventArgs e)
+        {
+            List<List<dbQuestionParmeters>> finalSimulation = new List<List<dbQuestionParmeters>>();
+            List<dbQuestionParmeters> currQuestions;
+            currQuestions = OperationsAndOtherUseful.sendChapter_english_Questions();
+            finalSimulation.Add(currQuestions);
+            currQuestions = OperationsAndOtherUseful.sendChapter_english_Questions();
+            finalSimulation.Add(currQuestions);
+            
+            currQuestions = OperationsAndOtherUseful.sendChapter_math_Questions();
+            finalSimulation.Add(currQuestions);
+            currQuestions = OperationsAndOtherUseful.sendChapter_math_Questions();
+            finalSimulation.Add(currQuestions);
+            
+            currQuestions = OperationsAndOtherUseful.sendChapter_hebrew_Questions();
+            finalSimulation.Add(currQuestions);
+            currQuestions = OperationsAndOtherUseful.sendChapter_hebrew_Questions();
+            finalSimulation.Add(currQuestions);
+            HtmlConvertOptionsMenu n = new HtmlConvertOptionsMenu(finalSimulation);
+            try { n.Show(); }
+            catch(Exception ex) { }
+            
         }
     }
 }
